fa4f68ea267ca6665389ba0c946b498412c0ac24
workwithraymond <workwithraymond01@gmail.com>
2024-02-20 06:48:21 -0500

    new changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,13 @@ const Login = () => {
      />
      <div className='mt-4'>
       <SubmitBtn text='login'/>
+     
      </div>
+     <button type = 'button' className='btn btn-secondary btn-block'>guest user</button>
+      <p className='text-center'> Not a member yet?
+        <Link to='/register' className='ml-2 link link-hover link-primary capitalize'/>
+      </p>
+
      </Form>
     </section>
   )
