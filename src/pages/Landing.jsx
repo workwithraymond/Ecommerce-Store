1ce249cc0cea5d7eecadc0036aad4862b630dc2a
workwithraymond <workwithraymond01@gmail.com>
2024-02-22 06:47:34 -0500

    new changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,12 @@
+
+
+import { Hero } from '../components'
+
 const Landing = () => {
     return (
-      <h1 className="text-4xl">Landing</h1>
+      <>
+      <Hero />
+      </>
     )
   }
   export default Landing
