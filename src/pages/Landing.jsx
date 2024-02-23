569767e8d71a6658cbcce107f73b0eed470690e3
workwithraymond <workwithraymond01@gmail.com>
2024-02-22 19:16:07 -0500

    new changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { Hero } from '../components';
+import { FeaturedProducts, Hero } from '../components';
 import { customFetch } from '../utils';
 
 const url = '/products?featured=true';
@@ -13,6 +13,7 @@ const Landing = () => {
     return (
       <>
       <Hero />
+      <FeaturedProducts />
       </>
     )
   }
