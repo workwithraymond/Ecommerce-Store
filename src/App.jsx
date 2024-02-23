bd2f5bf08886f0607323ad3aec3e68bfddc34756
workwithraymond <workwithraymond01@gmail.com>
2024-02-23 07:27:44 -0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { ErrorElement } from './components';
 
 // loaders
 import { loader as landingLoader} from './pages/Landing'  
+import { loader as singleProductLoader} from './pages/SingleProduct' 
 // actions
 
 const router = createBrowserRouter([
@@ -27,6 +28,8 @@ const router = createBrowserRouter([
       {
         path:'products/:id',
         element:<SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader:singleProductLoader,
       },
       {
         path:'cart',
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -13,4 +13,15 @@ export const formatPrice = (price) => {
         currency:'USD',
     }).format((price / 100).toFixed(2));
     return dollarsAmount;
+};
+
+export const generateAmountOptions = (number) => {
+    return  Array.from({length:number}, (_,index) => {
+            const amount = index +1
+            return (
+                <option key={amount} value={amount}>{amount}</option>
+            )
+            
+        })
+    
 }
